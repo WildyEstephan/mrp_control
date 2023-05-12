0c5f0bd29ffe3c385c80907699d4173b3bc6c8c2
Wildy Estephan <westephan50@gmail.com>
2023-05-12 17:44:39 -0400

    Add business logic and views 3

--- mrp_control_qty_production/models/stock_move_line.py
+++ mrp_control_qty_production/models/stock_move_line.py
@@ -4,6 +4,6 @@ from odoo import api, fields, models
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    use_history_controll = fields.Boolean(
+    use_history_control = fields.Boolean(
         string='Use history control',
         required=False, related='production_id.use_history_control')
